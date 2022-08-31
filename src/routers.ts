56f9a23b51d3bdf18c5675c1279aeb64ec95ebfc
Cristovão <cristovao.s.lima@gmail.com>
2022-08-31 16:54:18 -0300

    product

--- src/routers.ts
+++ src/routers.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
@@ -8,7 +9,13 @@ import { Authenticated } from './middlewares/Authenticated';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 
+import { CreateProductController } from './controllers/product/CreateProductController';
+
+import uploadConfig from './config/multer';
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 // Rotas user
 router.post('/user', new CreateUserController().handle);
@@ -21,5 +28,10 @@ router.post('/category', Authenticated, new CreateCategoryController().handle);
 router.put('/category', Authenticated, new UserController().handle);
 router.delete('/category', Authenticated, new UserController().handle);
 
+// Rotas product
+router.get('/product', Authenticated, new ListCategoryController().handle);
+router.post('/product', Authenticated, upload.single('file'), new CreateProductController().handle);
+router.put('/product', Authenticated, new UserController().handle);
+router.delete('/product', Authenticated, new UserController().handle);
 
 export { router };
---
+++ src/services/product/CreateProductService.ts
@@ -0,0 +1,33 @@
+import prismaClient from "../../prisma";
+
+interface ProductRequest {
+    name: String;
+    price: String;
+    description: String;
+    banner: String;
+    category_id: String;
+}
+
+class CreateProductService {
+    async execute({name, price, description, banner, category_id}: ProductRequest){
+        const product = await prismaClient.product.create({
+            data: {
+                name: name,
+                price: price,
+                description: description,
+                banner: banner,
+                category_id: category_id
+            },
+            select: {
+                id: true,
+                name: true,
+                price: true,
+                description: true,
+                banner: true
+            }
+        });
+        return product;
+    }
+}
+
+export { CreateProductService };
